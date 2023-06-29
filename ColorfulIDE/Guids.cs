5dc89a58f074f482d04b461619bcf600c2822aa3
sisiler <138165925+sisiler@users.noreply.github.com>
2023-06-29 14:02:33 -0700

    Add files via upload

---
+++ ColorfulIDE/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace HiTeam.ColorfulIDE
+{
+    static class GuidList
+    {
+        public const string GuidColorfulIdePkgString = "6f012ab1-a7eb-48ec-a09a-344442bf17e2";
+        public const string GuidColorfulIdeCmdSetString = "a436d208-80da-4c94-b22e-90f6d344035b";
+
+        public static readonly Guid GuidColorfulIdeCmdSet = new Guid(GuidColorfulIdeCmdSetString);
+    };
+}
---
+++ ColorfulIDE/Options/LocalizedDescription.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace HiTeam.ColorfulIDE.Options
+{
+    class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        public LocalizedDescriptionAttribute(string resourceId)
+            : base(GetMessageFromResource(resourceId))
+        { }
+
+        private static string GetMessageFromResource(string resourceId)
+        {
+            // TODO: Return the string from the resource file
+            return Properties.Resources.ResourceManager.GetString(resourceId);
+        }
+    }
+}
